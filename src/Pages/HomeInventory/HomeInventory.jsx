5d1dce76fba5aa9bb4985a97e20fbb8ef291c687
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-04-30 20:06:48 +0600

    card details updated

--- src/Pages/HomeInventory/HomeInventory.jsx
+++ src/Pages/HomeInventory/HomeInventory.jsx
@@ -2,19 +2,24 @@ import React from "react";
 import { Card } from "react-bootstrap";
 
 const HomeInventory = ({ car }) => {
-  const { name, img, des, price } = car;
+  const { name, img, des, price, quantity, supplier } = car;
   return (
     <div className="col-md-4 col-12 my-3">
       <Card>
         <Card.Img variant="top" src={img} />
         <Card.Body>
-          <Card.Title>{name}</Card.Title>
-          <Card.Text>{des.slice(0, 100) + "..."}</Card.Text>
-        </Card.Body>
-        <Card.Footer>
+          <Card.Title className="fw-bold">{name}</Card.Title>
+          <Card.Text className="text-secondary">
+            {des.slice(0, 100) + "..."}
+          </Card.Text>
+          <Card.Text className="text-secondary">Quantity: {quantity}</Card.Text>
+          <Card.Text className="text-secondary">Supplier: {supplier}</Card.Text>
           <small className="text-muted">
             Starting at <span className="fw-bold">${price}</span>
           </small>
+        </Card.Body>
+        <Card.Footer>
+          <button className="btn btn-primary d-block w-100">Update</button>
         </Card.Footer>
       </Card>
     </div>
