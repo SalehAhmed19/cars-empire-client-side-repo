fc9eecdcbb4b91bd61ff8526945f07ad973418d1
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-05-06 20:38:31 +0600

    access token issues fix

--- src/Hooks/useMyItems.js
+++ src/Hooks/useMyItems.js
@@ -1,21 +1,31 @@
 import axios from "axios";
+import { signOut } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 import auth from "../firebase.init";
 
 const useMyItems = () => {
   const [user] = useAuthState(auth);
   const [myItems, setMyItems] = useState([]);
+  const navigate = useNavigate();
   useEffect(() => {
     const getMyItem = async () => {
       const email = user?.email;
       const url = `https://protected-lake-29761.herokuapp.com/my-items?email=${email}`;
-      const { data } = await axios.get(url, {
-        headers: {
-          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        },
-      });
-      setMyItems(data);
+      try {
+        const { data } = await axios.get(url, {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        });
+        setMyItems(data);
+      } catch (error) {
+        if (error.response.status === 403 || error.response.status === 403) {
+          signOut(auth);
+          navigate("/login");
+        }
+      }
     };
     getMyItem();
   }, [user]);
