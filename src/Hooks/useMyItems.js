bfb5b7b359e7d55e7395df8f61aeebfb46a4a8b3
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-05-06 20:46:57 +0600

    access token issues fix

--- src/Hooks/useMyItems.js
+++ src/Hooks/useMyItems.js
@@ -21,7 +21,7 @@ const useMyItems = () => {
         });
         setMyItems(data);
       } catch (error) {
-        if (error.response.status === 403 || error.response.status === 403) {
+        if (error.response.status === 403 || error.response.status === 401) {
           signOut(auth);
           navigate("/login");
         }
